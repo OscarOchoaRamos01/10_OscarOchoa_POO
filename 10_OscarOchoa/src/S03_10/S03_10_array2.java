625a8fb7cdacde26f32ba6590f8be3d786d5d6c7
OscarOchoaRamos01 <oscar.ochoa.r@vallegrande.edu.pe>
2024-03-11 23:42:53 -0500

    array 2

--- 10_OscarOchoa/src/S03_10/S03_10_array2.java
+++ 10_OscarOchoa/src/S03_10/S03_10_array2.java
@@ -11,7 +11,7 @@ package S03_10;
  */
 public class S03_10_array2 {
     public static void main(String[] args){
-        int [][] numeros2 = {{15, 30},{20, 40}};
+        int [][] numeros2 = {{15, 30},{20, 60}};
 
         System.out.println(numeros2 [1][0]);
 
